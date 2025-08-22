18f59fc76f13d6ceb00ac068103d2d5060f85650
Iffy <1ffycatx@gmail.com>
2025-08-22 12:06:17 +0800

    Added default.nix and flake.nix

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Telegram bot CLI";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    {
+      defaultPackage = pkgs.callPackage ./default.nix { };
+    });
+}
